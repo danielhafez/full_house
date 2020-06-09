042621ba2b92f5f01548073d21f14fdec7960eba
Daniel <hafez.daniel@gmail.com>
2020-06-09 12:31:41 +0300

    changes on the register page

--- FE/full_house/src/components/Register.jsx
+++ FE/full_house/src/components/Register.jsx
@@ -6,12 +6,13 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import ButtonSuccess from './ButtonSuccess';
 // import CheckboxesGroup from './Checkbox';
 // import CheckboxesDesiredSkills from './CheckboxDesiredSkills';
 // import CourseSelect from './Courses';
 // // import { createStudent } from '../lib/api';
 import { Link } from 'react-router-dom';
-// import ButtonSuccess from './ButtonSuccess';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
 
@@ -20,7 +21,7 @@ function Copyright() {
     <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
       <Link color='inherit' to='https://material-ui.com/'>
-        Daniel's Hogwarts CRM
+        Group #1 Hackathon 2020
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -104,65 +105,66 @@ class Register extends React.Component {
         className='add-student-container'
         maxWidth='md'
       >
-        <CssBaseline />
-        <div className='paper'>
-          <img
-            src='https://static.thenounproject.com/png/7729-200.png'
-            alt='harry potter icon'
-            className='signup-logo'
-          ></img>
-          <Typography component='h1' variant='h3' className='signup-title'>
-            Hogwarts student portal
-          </Typography>
+        <Card clas>
+          {' '}
+          <CssBaseline />
+          <div className='paper'>
+            <img
+              src='https://static.thenounproject.com/png/7729-200.png'
+              alt='harry potter icon'
+              className='signup-logo'
+            ></img>
+            <Typography component='h1' variant='h3' className='signup-title'>
+              FULL HOUSE{' '}
+            </Typography>
 
-          <Snackbar open={this.state.success} autoHideDuration={1000}>
-            <Alert
-              // onClose={this.setState({ success: false })}
-              autoHideDuration={1000}
-              severity='success'
-            >
-              Student adedd successfully!
-            </Alert>
-          </Snackbar>
-          {/* {this.state.success && (
+            <Snackbar open={this.state.success} autoHideDuration={1000}>
+              <Alert
+                // onClose={this.setState({ success: false })}
+                autoHideDuration={1000}
+                severity='success'
+              >
+                Organization adedd successfully!
+              </Alert>
+            </Snackbar>
+            {/* {this.state.success && (
            
           )} */}
-          <Typography component='h1' variant='h5'>
-            Add a student{' '}
-          </Typography>
-          <form className='form' noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete='fname'
-                  onChange={(event) => {
-                    this.setState({ first_name: event.target.value });
-                  }}
-                  name='firstName'
-                  variant='outlined'
-                  required
-                  fullWidth
-                  id='firstName'
-                  label='First Name'
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant='outlined'
-                  required
-                  fullWidth
-                  id='lastName'
-                  label='Last Name'
-                  name='lastName'
-                  autoComplete='lname'
-                  onChange={(event) => {
-                    this.setState({ last_name: event.target.value });
-                  }}
-                />
-              </Grid>
+            <Typography component='h1' variant='h5'>
+              Register your organization{' '}
+            </Typography>
+            <form className='form' noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    onChange={(event) => {
+                      this.setState({ first_name: event.target.value });
+                    }}
+                    name='companyName'
+                    variant='outlined'
+                    required
+                    fullWidth
+                    id='companyName'
+                    label='Organization Name'
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    variant='outlined'
+                    required
+                    fullWidth
+                    id='lastName'
+                    label='Last Name'
+                    name='lastName'
+                    autoComplete='lname'
+                    onChange={(event) => {
+                      this.setState({ last_name: event.target.value });
+                    }}
+                  />
+                </Grid>
 
-              {/* <Grid item xs={12} sm={6}>
+                {/* <Grid item xs={12} sm={6}>
                 <CheckboxesGroup passToMain={this.handleDataMagicSkills} />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -173,16 +175,17 @@ class Register extends React.Component {
               <Grid item xs={11}>
                 <CourseSelect passToSignup={this.handleDataCourses} />
               </Grid> */}
-            </Grid>
-            {/* <ButtonSuccess submit={this.submit}></ButtonSuccess> */}
-            <Grid container justify='flex-end'>
-              <Grid item></Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={5} mb={20} pb={5}>
-          <Copyright />
-        </Box>
+              </Grid>
+              <ButtonSuccess submit={this.submit}></ButtonSuccess>
+              <Grid container justify='flex-end'>
+                <Grid item></Grid>
+              </Grid>
+            </form>
+          </div>
+          <Box mt={5} mb={20} pb={5}>
+            <Copyright />
+          </Box>
+        </Card>
       </Container>
     );
   }
