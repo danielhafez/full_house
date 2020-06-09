ad890069555b4e5b0bc0cef750eddc15cda6b56f
Daniel <hafez.daniel@gmail.com>
2020-06-09 11:03:46 +0300

    path register app.js

--- FE/full_house/src/App.js
+++ FE/full_house/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import LandingPage from "./components/LandingPage";
-import Login from "./components/Login";
-import NavBar from "./components/NavBar";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <NavBar />
         <LandingPage />
       </div>
-      <Route path="/login">
+      <Route path='/login'>
         <Login />
       </Route>
+      <Route path='/register'></Route>
     </Router>
   );
 }
