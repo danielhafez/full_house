215aeb0c4fa6f327cc9b4a2e51db884e8b5d4e2b
Daniel <hafez.daniel@gmail.com>
2020-06-09 23:33:00 +0300

    created business dashboard

---
+++ FE/full_house/src/components/Deposits.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Title from './Title';
+import SendFrame from '../components/SendFrame';
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+});
+
+export default function Deposits() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Current Occupancy</Title>
+      <Typography component='p' variant='h4'>
+        90%{' '}
+      </Typography>
+      <Typography color='textSecondary' className={classes.depositContext}>
+        Last Update at: {new Date().getHours()}:00
+      </Typography>
+      <div>
+        <Link color='primary' href='#' onClick={preventDefault}>
+          View report
+        </Link>
+        <SendFrame></SendFrame>
+      </div>
+    </React.Fragment>
+  );
+}
