18de39cee912c4459227e458f3b784271878269e
gabe <gabrielbruck1@gmail.com>
2020-06-09 18:27:49 +0300

    added link

--- FE/full_house/src/components/BusinessProfile.jsx
+++ FE/full_house/src/components/BusinessProfile.jsx
@@ -1,74 +1,9 @@
-import React from "react";
-import MaterialTable from "material-table";
-import { getAllCompanies } from "../lib/api";
+import React from 'react'
 
-export default function BusinessSearch() {
-  const [state, setState] = React.useState({
-    columns: [
-      { title: "Logo", field: "logo" },
-      { title: "Company Name", field: "company_name" },
-    ],
-    data: [],
-  });
-
-  ccomponentDidMount = () => {
-    getCompanies();
-  };
-
-  const getCompanies = () => {
-    getAllCompanies()
-      .then((response) => {
-        const companiesdata = response.data;
-        this.setState({ data: companiesdata });
-        return response.json;
-      })
-      .catch(() => {
-        alert("Retriving data");
-      });
-  };
-
+export default function BusinessProfile() {
   return (
-    <MaterialTable
-      title="Editable Example"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
-  );
+    <div>
+      
+    </div>
+  )
 }
