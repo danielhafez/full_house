9cf7a96066d014666edcd93cd16322486d26a035
gabe <gabrielbruck1@gmail.com>
2020-06-09 10:37:52 +0300

    first commit

---
+++ FE/full_house/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <LandingPage />
+      </div>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
---
+++ FE/full_house/src/components/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#ffff", height: "50vh" }}
+        >
+          <img src="../images/Logo-fullhouse.png" alt="logo_fullHouse" />
+        </Typography>
+        <div>
+          <Button variant="outlined" color="secondary">
+            How full is your favourite place?
+          </Button>
+        </div>
+      </Container>
+    </React.Fragment>
+  );
+};
+export default LandingPage;
