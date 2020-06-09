287552f6dba311c988b3691d63ae6d3dcf503f0d
Daniel <hafez.daniel@gmail.com>
2020-06-09 11:23:34 +0300

    nothing

--- FE/full_house/src/App.js
+++ FE/full_house/src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import LandingPage from "./components/LandingPage";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import NavBar from "./components/NavBar";
-import BusinessSearch from "./components/BusinessSearch";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Register from './components/Register';
+import NavBar from './components/NavBar';
+import BusinessSearch from './components/BusinessSearch';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <NavBar />
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path='/' component={LandingPage} />
         <div>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/all_companies" component={BusinessSearch} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/all_companies' component={BusinessSearch} />
         </div>
       </div>
     </Router>
--- FE/full_house/src/components/LandingPage.jsx
+++ FE/full_house/src/components/LandingPage.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth='sm'>
         <Typography
-          component="div"
-          style={{ backgroundColor: "#ffff", height: "50vh" }}
+          component='div'
+          style={{ backgroundColor: '#ffff', height: '50vh' }}
         >
-          <img src="../images/Logo-fullhouse.png" alt="logo_fullHouse" />
+          <img src='../images/Logo-fullhouse.png' alt='logo_fullHouse' />
         </Typography>
         <div>
           <Button
-            variant="outlined"
-            color="secondary"
+            variant='outlined'
+            color='secondary'
             onClick={LookForBusiness}
           >
             How full is your favourite place?
           </Button>
           <Button
-            variant="outlined"
-            color="secondary"
+            variant='outlined'
+            color='secondary'
             onClick={LookForBusiness}
           >
             Register your Business now!
