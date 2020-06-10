40e7017171ddee285df771b8da0b0777e4646506
gabe <gabrielbruck1@gmail.com>
2020-06-10 09:50:54 +0300

    added dashboard to company page

--- FE/full_house/src/App.js
+++ FE/full_house/src/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import './css/register.css';
-import LandingPage from './components/LandingPage';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import NavBar from './components/NavBar';
-import BusinessSearch from './components/BusinessSearch';
-import BusinessProfile from './components/BusinessProfile';
-import SendFrame from './components/SendFrame';
-import Dashboard from './components/BusinessDashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "./css/register.css";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import NavBar from "./components/NavBar";
+import BusinessSearch from "./components/BusinessSearch";
+import BusinessProfile from "./components/BusinessProfile";
+import SendFrame from "./components/SendFrame";
+import Dashboard from "./components/BusinessDashboard";
 
 function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <NavBar />
-        <Route exact path='/' component={LandingPage} />
+        <Route exact path="/" component={LandingPage} />
         <div>
           {/* <Route exact path='/login' component={Login} /> */}
-          <Route exact path='/register' component={Signup} />
-          <Route exact path='/all_companies' component={BusinessSearch} />
-          <Route exact path='/business_profile' component={BusinessProfile} />
-          <Route exact path='/send-frame' component={SendFrame} />
+          <Route exact path="/register" component={Signup} />
+          <Route exact path="/all_companies" component={BusinessSearch} />
+          <Route exact path="/business_profile" component={BusinessProfile} />
+          <Route exact path="/send-frame" component={SendFrame} />
           <Switch>
-            <Route path='/business-dashboard/:id' children={<Dashboard />} />
+            <Route path="/business-dashboard/:id" children={<Dashboard />} />
           </Switch>
           <Switch>
             <Route
               exact
-              path='/companyprofile:id'
-              name='companyprofile'
+              path="/companyprofile/:id"
+              name="companyprofile"
               children={<BusinessProfile />}
             />
           </Switch>
--- FE/full_house/src/components/BusinessProfile.jsx
+++ FE/full_house/src/components/BusinessProfile.jsx
@@ -6,6 +6,11 @@ import Container from "@material-ui/core/Container";
 import "../css/BusinessProfile.css";
 import { useParams } from "react-router-dom";
 import { searchCompany } from "../lib/api";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Chart from "./Chart";
+import Deposits from "./Deposits";
+import clsx from "clsx";
 
 const BusinessProfile = () => {
   let { id } = useParams();
@@ -18,48 +23,55 @@ const BusinessProfile = () => {
   }, [company]);
 
   return (
-    <Container maxWidth="sm" style={{ marginTop: "40px" }}>
-      <Card style={{ margin: "10%" }} boxShadow={3}>
-        <CardContent>
-          <img
-            src={company.logo}
-            alt="company_logo"
-            style={{ width: "150px", height: "120px", marginBottom: "-20px" }}
-          />
-          <h1>{company.company_name}</h1>
+    <div className="background">
+      <Container maxWidth="sm" style={{ marginTop: "40px" }}>
+        <Card className="card_styling">
+          <CardContent>
+            <img
+              src={company.logo}
+              alt="company_logo"
+              style={{ width: "150px", height: "120px", marginBottom: "-20px" }}
+            />
+            <h1>{company.company_name}</h1>
 
-          <Typography>
-            Maximum Capacity Allowed: {company.maximum_capacity}
-          </Typography>
-        </CardContent>
-      </Card>
-      <Card className="business_information">
-        <CardContent>
-          <h2>Business Information:</h2>
-          <div className="business_content">
-            <b style={{ marginRight: "10px" }}>Business Address: </b>
-            {company.address}
-          </div>
-          <div className="business_content">
-            <b style={{ marginRight: "10px" }}>Phone Number: </b>
-            {company.phone}
-          </div>
-          <div className="business_content">
-            <b>Business Information:</b>
-          </div>
-          <div>{company.description}</div>
-        </CardContent>
-      </Card>
-    </Container>
+            <Typography>
+              Maximum Capacity Allowed: {company.maximum_capacity}
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <Card className="card_styling">
+          <CardContent>
+            <Deposits occupancy={80} />
+          </CardContent>
+        </Card>
+        <Card>
+          <Paper className="fixed-height">
+            <Chart />
+          </Paper>
+        </Card>
+        <Card className="business_information card_styling">
+          <CardContent>
+            <h2>Business Information:</h2>
+            <div className="business_content">
+              <b style={{ marginRight: "10px" }}>Business Address: </b>
+              {company.address}
+            </div>
+            <div className="business_content">
+              <b style={{ marginRight: "10px" }}>Phone Number: </b>
+              {company.phone}
+            </div>
+            <div className="business_content">
+              <b>Business Information:</b>
+            </div>
+            <div>{company.description}</div>
+          </CardContent>
+        </Card>
+      </Container>
+      {/* Chart */}
+      {/* % occupancy */}
+    </div>
   );
 };
 
 export default BusinessProfile;
-
-// export default function BusinessProfile() {
-//   return (
-//     <>
-
-//     </>
-//   );
-// }
