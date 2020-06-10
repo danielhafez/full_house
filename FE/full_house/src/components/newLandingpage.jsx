691415f7f7acc0de629e94189734db8d8dbc194d
Daniel <hafez.daniel@gmail.com>
2020-06-10 15:09:08 +0300

    added names to the footer

--- FE/full_house/src/components/newLandingpage.jsx
+++ FE/full_house/src/components/newLandingpage.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { Link } from "react-router-dom";
-import logo from "../images/logo.png";
-import image from "../images/business.png";
-import hospital from "../images/hospital.png";
-import institution from "../images/Institution-512.png";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+import image from '../images/business.png';
+import hospital from '../images/hospital.png';
+import institution from '../images/Institution-512.png';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://material-ui.com/'>
         Group#1 Hackathon 2020
-      </Link>{" "}
-      {"."}
+      </Link>{' '}
+      {'.'}
     </Typography>
   );
 }
@@ -46,12 +46,12 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(8),
   },
   card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
   cardMedia: {
-    paddingTop: "56.25%", // 16:9
+    paddingTop: '56.25%', // 16:9
   },
   cardContent: {
     flexGrow: 1,
@@ -73,12 +73,12 @@ const NewLandingPage = () => {
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <img src={logo} alt="logo" className="logo" />
+          <Container maxWidth='sm'>
+            <img src={logo} alt='logo' className='logo' />
             <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
+              variant='h5'
+              align='center'
+              color='textSecondary'
               paragraph
             >
               An AI powered system that allows you to plan your day safely. Full
@@ -86,30 +86,30 @@ const NewLandingPage = () => {
               organizations and keeps the world safer
             </Typography>
             <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
+              <Grid container spacing={2} justify='center'>
                 <Grid item>
-                  <Button variant="contained" color="primary">
-                    <Link style={{ color: "white" }} to="/all_companies">
+                  <Button variant='contained' color='primary'>
+                    <Link style={{ color: 'white' }} to='/all_companies'>
                       Explore
                     </Link>
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button variant="outlined" color="primary">
-                    <Link to="/register"> Register your organization now!</Link>
+                  <Button variant='outlined' color='primary'>
+                    <Link to='/register'> Register your organization now!</Link>
                   </Button>
                 </Grid>
               </Grid>
             </div>
           </Container>
         </div>
-        <Container className={classes.cardGrid} maxWidth="md">
+        <Container className={classes.cardGrid} maxWidth='md'>
           {/* End hero unit */}
           <Typography
-            component="h1"
-            variant="h4"
-            align="center"
-            color="textPrimary"
+            component='h1'
+            variant='h4'
+            align='center'
+            color='textPrimary'
             gutterBottom
           >
             Use Cases
@@ -120,10 +120,10 @@ const NewLandingPage = () => {
                 <CardMedia
                   className={classes.cardMedia}
                   image={image}
-                  title="Image title"
+                  title='Image title'
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
+                  <Typography gutterBottom variant='h5' component='h2'>
                     Businesses
                   </Typography>
                   <Typography>
@@ -132,7 +132,7 @@ const NewLandingPage = () => {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button align="center" size="small" color="primary">
+                  <Button align='center' size='small' color='primary'>
                     Register Now
                   </Button>
                 </CardActions>
@@ -144,10 +144,10 @@ const NewLandingPage = () => {
                 <CardMedia
                   className={classes.cardMedia}
                   image={hospital}
-                  title="Image title"
+                  title='Image title'
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
+                  <Typography gutterBottom variant='h5' component='h2'>
                     Emergency Care
                   </Typography>
                   <Typography>
@@ -157,8 +157,8 @@ const NewLandingPage = () => {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" color="primary">
-                    Learn More{" "}
+                  <Button size='small' color='primary'>
+                    Learn More{' '}
                   </Button>
                 </CardActions>
               </Card>
@@ -169,10 +169,10 @@ const NewLandingPage = () => {
                 <CardMedia
                   className={classes.cardMedia}
                   image={institution}
-                  title="Image title"
+                  title='Image title'
                 />
                 <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
+                  <Typography gutterBottom variant='h5' component='h2'>
                     Public Office
                   </Typography>
                   <Typography>
@@ -181,7 +181,7 @@ const NewLandingPage = () => {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" color="primary">
+                  <Button size='small' color='primary'>
                     Learn More
                   </Button>
                 </CardActions>
@@ -192,16 +192,16 @@ const NewLandingPage = () => {
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
+        <Typography variant='h6' align='center' gutterBottom>
+          FullHouse
         </Typography>
         <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
+          variant='subtitle1'
+          align='center'
+          color='textSecondary'
+          component='p'
         >
-          Something here to give the footer a purpose!
+          Gabriel Bruck, Daniel Hafez, Or Gindes, Dor Sklar
         </Typography>
         <Copyright />
       </footer>
